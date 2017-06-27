b1c1896a04a7fc50307d8e825a00cf48e0eb1bd7
Adam <acdoyle630@gmail.com>
2017-06-27 11:10:14 -1000

    darts in python

--- python-dart/dart.py
+++ python-dart/dart.py
@@ -1,4 +1,17 @@
 def dart ( arr ):
-  print arr
+  score = 0
+  perfect = 0
+  length = len(arr)
+  for x in range (0, length):
+    if arr[x] < 5 and arr[x] >= 0:
+      score += 10
+      perfect += 1
+    if arr[x] <= 10 and arr[x] >= 5:
+      score += 5
+    if perfect == length:
+      score += 100
 
-dart([1,2,3])
+  print score
+  print perfect
+
+dart([1,2,3,7,10])
